15d3f3de554455066cc2ef2a33c90fb60dc782d7
Mateus <mateus.silvainfo@gmail.com>
2024-02-17 21:08:19 -0300

    updated footer text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-950 py-10 font-inter">
       <p className="text-center text-lg text-neutral-200">
-        Todos os direitos reservados.
+       All rights reserved.
       </p>
     </footer>
   );
